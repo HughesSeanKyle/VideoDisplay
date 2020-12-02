0212143f92e31cd0b7e7af638d47c71d39355d91
HughesSeanKyle <khughessean@yahoo.com>
2020-12-02 16:24:22 +0200

    V121 - Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,8 +1,24 @@
+import axios from 'axios';
+
 import { youtubeKey } from '../config/keys';
 
 const KEY = youtubeKey;
 
+export default axios.create({
+    baseURL: 'http://www.googleapis.com/youtube/v3', 
+    params: {
+        part: 'snippets', 
+        maxResults: 5,
+        key: KEY
+    }
+});
+
 /*
 Captital letters for variable is convention
     indicated that this is a constant value and it should not be changed. 
+
+V121 - snippet - Indicates what type of information we want to retrieve from the call.
+    max result - How much items we want to retrieve
+    Q = query - search term we will make
 */
+
