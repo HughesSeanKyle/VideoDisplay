461eced04285831d9ca8c8b04ccad558ca0ee3b2
HughesSeanKyle <khughessean@yahoo.com>
2020-12-02 15:23:05 +0200

    V118 - Handling Form Submission

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,22 @@ import React from 'react';
  class SearchBar extends React.Component {
     state = { term: '' };
 
-    onInputChange = (e) => {
-        console.log(e.target.value);
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault(); // Prevent submission on enter key down
+
+        // TODO: Make sure to call
+        // callback from parent. 
+            // To tell app user submitted form and want to initiate new search. 
     };
 
      render() {
          return ( 
          <div className="search-bar ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                     <label>Video Search</label>
                     <input 
