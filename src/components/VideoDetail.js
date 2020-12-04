10d205cafdd51a2358b0febd081bddaceafdc8c4
HughesSeanKyle <khughessean@yahoo.com>
2020-12-04 15:31:58 +0200

    V133. Displaying a Video Player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,9 +4,14 @@ const VideoDetail = ({ video }) => {
     if (!video) { // 1
         return <div>Loading...</div>;
     };
+
+    const videoSrc = `http://www.youtube.com/embed/${video.id.videoId}`;
     
     return (
         <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
             <div className="ui segment">
                 <h4>{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
@@ -21,5 +26,7 @@ export default VideoDetail;
 This is the component where the selected video will render. 
 
 note 1 - If no video then return div with loading text. Initially state is null.  
+
+iframe tag will request all styling done for the video player component. 
 
 */
