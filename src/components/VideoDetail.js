5ccb017b0a01f7bab9fc91ae7c4155a042aa5b9e
HughesSeanKyle <khughessean@yahoo.com>
2020-12-04 15:18:37 +0200

    V132 - Styling the video detail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,7 +5,14 @@ const VideoDetail = ({ video }) => {
         return <div>Loading...</div>;
     };
     
-    return <div>{video.snippet.title}</div>
+    return (
+        <div>
+            <div className="ui segment">
+                <h4>{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
+    );
 };
 
 export default VideoDetail;
@@ -13,6 +20,6 @@ export default VideoDetail;
 /*
 This is the component where the selected video will render. 
 
-note 1 - If no video then return div with loading text. Initially state is null. 
+note 1 - If no video then return div with loading text. Initially state is null.  
 
 */
