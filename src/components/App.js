5221f0e0a41936764b3aef65c0f18cc5093e4021
HughesSeanKyle <khughessean@yahoo.com>
2020-12-04 16:00:48 +0200

    V135 - Defaulting Video Selection

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,12 @@ import youtube from '../apis/youtube';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
+
+    // 6
+    componentDidMount() {
+        this.onTermSubmit('buildings');
+    };
+
     // Func will be called when search term submitted      
     onTermSubmit = async term => {
         //2
@@ -15,7 +21,10 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0] //5
+        });
     };
     
     // Callback for when video selected //3
@@ -53,6 +62,10 @@ note 3 - onVideoSelect={this.onVideoSelect} => propName = {this.callback}
        - this.setState({ selectedVideo: video }) - The video received from videoItem when       clicked on
 note 4 - In videoList we are sending an array of videos. Hence, prop name videos(plural)
        - In VideoDetail we are passing down a single video. Hence, prop name video(singular)
+note 5 - On search return just use first result as default value for state. 
+       - Ensures that that video apears on screen after search. 
 
+note 6 - component did mount method. will 
+       - attempt to make some default search when the app component is first rendered to the screen.
 
 */
